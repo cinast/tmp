209fdfb7a8151e4b055f46ec6c6bb22cee5a9650
Cosider <126345646+cinast@users.noreply.github.com>
2026-02-01 12:52:40 +0800

    Delete cocos-svg-series directory

--- cocos-svg-series/i18n/en.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-module.exports = {
-    description: "A series of component, make stupid Cocos know what is Scalable Vector Graphics",
-    test: "test",
-    "import-svg": "import svg",
-    "render-svg-preview": "render svg preview",
-};
